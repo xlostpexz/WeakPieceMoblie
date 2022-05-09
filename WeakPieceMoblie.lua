1b9251be40017f6850e8b105d6feb39ff4e92244
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-05-09 11:04:16 +0700

    Update WeakPieceMoblie.lua

--- WeakPieceMoblie.lua
+++ WeakPieceMoblie.lua
@@ -923,6 +923,59 @@ local Home2 = DinoPage2:NewSection("Misc")
 local Home3 = DinoPage3:NewSection("Teleport")
 local Home4 = DinoPage4:NewSection("Auto Eqiup")
 
+    Home:CreateToggle("Kill All",function(value)
+       _G.Killall = value
+while _G.Killall do wait()
+    local bosses = {workspace["[JOR]"],workspace["[MRMS]"],workspace["[Ace]"],workspace.Heaven}
+
+task.spawn(function()
+    while wait(1) do
+        pcall(function()
+            for _,v in pairs(bosses) do
+                fireclickdetector(v.ClickDetector)
+            end
+            for i,v2 in pairs(game.Players:GetChildren()) do
+                for _,v in pairs(v2.Backpack:GetChildren()) do
+                    if v:FindFirstChild("DF") and v:FindFirstChild("Event") then
+                        if v.Name == "Explosion" then
+                            v.Event:FireServer("Explosive")
+                        else
+                            v.Event:FireServer(v.Name.."Punch")
+                            v.Event:FireServer(v.Name)
+                        end
+                    end
+                end
+                if v2.Character then
+                    for _,v in pairs(v2.Character:GetChildren()) do
+                        if v:FindFirstChild("DF") and v:FindFirstChild("Event") then
+                            if v.Name == "Explosion" then
+                                v.Event:FireServer("Explosive")
+                            else
+                                v.Event:FireServer(v.Name.."Punch")
+                                v.Event:FireServer(v.Name)
+                            end
+                        end
+                    end
+                end
+            end
+        end)
+    end
+end)
+
+while wait(0.2) do
+    for i,v2 in pairs(workspace.Enemy:GetChildren()) do
+        for _,v in pairs(v2:GetChildren()) do
+            if v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
+                game:GetService("ReplicatedStorage").Remote.Skills:FireServer("MMExplosion", 9e18, v.HumanoidRootPart.CFrame)
+                game:GetService("ReplicatedStorage").Remote.Skills:FireServer("MExplosion", 9e18, v.HumanoidRootPart.CFrame)
+                game:GetService("ReplicatedStorage").Remote.Skills:FireServer("Explosion", 9e18, v.HumanoidRootPart.CFrame)
+            end
+        end
+    end
+end
+    end
+    end)
+    
     Home:CreateToggle("Auto Sea Warriors",function(value)
        _G.SW = value
        _G.NoClip = value
